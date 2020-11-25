edfe804d261ce1c636e56329347157c3c63d576a
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-11-25 09:04:12 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a1044e50210612f21c21.js"
+    "url": "webpack-runtime-5c9ca3f18f85fe507609.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1bc8899552f7531f3c0edd91447dc9fd"
+    "revision": "4ab507e7fb3a7fe6d79476722a380829"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "5fced74bd70225a6363988b2ea350a3e"
+    "revision": "a389c5fc40f8b13b47004ec8c527fe85"
   },
   {
     "url": "polyfill-ca8adf9770cfeec4c092.js"
