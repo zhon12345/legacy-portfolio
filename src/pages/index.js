9f465861651d4e18c4904e3776da163e75f3b862
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-11-25 08:56:22 +0800

    📦 Updated Meta Tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,21 @@ export default () => {
   return (
     <>
       <Helmet>
-				<meta charSet="utf-8" />
-				<title>Ng Zhun Onn</title>
-				<html lang='en' />
-				<meta name="description" content="Ng Zhun Onn's personal portfolio." />
+        <title>Ng Zhun Onn - A Full Time Student and a JavaScipt Developer</title>
+        <meta name="title" content="Ng Zhun Onn - A Full Time Student and a JavaScipt Developer"/>
+        <meta name="description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who has created projects such as Tavern Keeper, Snake Game and a simple Calculator."/>
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://zhon12345.github.io/"/>
+        <meta property="og:title" content="Ng Zhun Onn - A Full Time Student and a JavaScipt Developer"/>
+        <meta property="og:description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who has created projects such as Tavern Keeper, Snake Game and a simple Calculator."/>
+        <meta property="og:image" content="https://imgur.com/a/W32lZE5"/>
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://zhon12345.github.io/"/>
+        <meta property="twitter:title" content="Ng Zhun Onn - A Full Time Student and a JavaScipt Developer"/>
+        <meta property="twitter:description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who has created projects such as Tavern Keeper, Snake Game and a simple Calculator."/>
+        <meta property="twitter:image" content="https://imgur.com/a/W32lZE5"/>
 			</Helmet>
       <App />
     </>
