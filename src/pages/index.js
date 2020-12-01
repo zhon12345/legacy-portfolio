9829f35abacbe670d7aa765bec33bb40555f2a23
Ng Zhun Onn <ngzhunonn@gmail.com>
2020-12-02 00:29:27 +0800

    📦 Updated site's metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,23 +9,23 @@ export default () => {
     return (
         <>
             <Helmet>
-                <title>Ng Zhun Onn - A Full Time Student and a JavaScript Developer</title>
+                <title>Ng Zhun Onn - Always learn, discover, and try new things</title>
                 <meta name="url" content="https://zhon12345.github.io/" />
-                <meta name="title" content="Ng Zhun Onn - A Full Time Student and a JavaScript Developer" />
-                <meta name="description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who has created projects such as Tavern Keeper, a Snake Game replica and a simple Calculator." />
-                <meta name="image" content="https://i.imgur.com/vyxCtv2.png" />
+                <meta name="title" content="Ng Zhun Onn - Always learn, discover, and try new things" />
+                <meta name="description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who is always learning, discovering, and trying new things!" />
+                <meta name="image" content="https://i.imgur.com/9mVpqHn.png" />
 
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://zhon12345.github.io/" />
-                <meta property="og:title" content="Ng Zhun Onn - A Full Time Student and a JavaScript Developer" />
-                <meta property="og:description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who has created projects such as Tavern Keeper, a Snake Game replica and a simple Calculator." />
-                <meta property="og:image:url" content="https://i.imgur.com/vyxCtv2.png" />
+                <meta property="og:title" content="Ng Zhun Onn - Always learn, discover, and try new things" />
+                <meta property="og:description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who is always learning, discovering, and trying new things!" />
+                <meta property="og:image:url" content="https://i.imgur.com/9mVpqHn.png" />
 
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://zhon12345.github.io/" />
-                <meta property="twitter:title" content="Ng Zhun Onn - A Full Time Student and a JavaScript Developer" />
-                <meta property="twitter:description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who has created projects such as Tavern Keeper, a Snake Game replica and a simple Calculator." />
-                <meta property="twitter:image" content="https://i.imgur.com/vyxCtv2.png" />
+                <meta property="twitter:title" content="Ng Zhun Onn - Always learn, discover, and try new things" />
+                <meta property="twitter:description" content="Ng Zhun Onn - A full-time student from Kuala Lumpur, Malaysia. Who is always learning, discovering, and trying new things!" />
+                <meta property="twitter:image" content="https://i.imgur.com/9mVpqHn.png" />
             </Helmet>
             <App />
         </>
